d5bfb67b3ba9548bf944ad073c2f87071c69a232
prapancadev <prapanca1987@gmail.com>
2024-10-24 18:35:28 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+      serverActions: {
+        allowedOrigins: [
+          'effective-rotary-phone-jjq94gjvjjgjfxrg-3000.app.github.dev', 'localhost:300'
+        ]
+      }
+    }
 };
 
 export default nextConfig;
