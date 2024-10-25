148217daf7f4060dd03664d8702e8ab3646a14c1
prapancadev <prapanca1987@gmail.com>
2024-10-25 08:25:46 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins: [
-        'localhost:300'
+        'effective-rotary-phone-jjq94gjvjjgjfxrg-3000.app.github.dev','localhost:300'
       ]
     },
   },
